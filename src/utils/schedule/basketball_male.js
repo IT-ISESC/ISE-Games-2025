5cb05f5b804f43dedd0898751941c10cdc9dfc19
Thames <chiratt.thames@gmail.com>
2025-03-25 01:34:32 +0700

    Fix: Bug fixes

--- src/utils/schedule/basketball_male.js
+++ src/utils/schedule/basketball_male.js
@@ -127,7 +127,7 @@ export const basketball_male = [
         winner: '',
         date: '9/4/2025',
         time: '19:00',
-        week: 'Finals',
+        week: '3',
         location: 'Court 1',
         status: 'On-going',
     },
